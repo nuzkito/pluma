f0d26f56f8c1ceea0c352e03921d33544f8a5bfc
Nuzkito <nuzkito+github@gmail.com>
2026-03-21 10:52:42 +0100

    Set up a default app key

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'base64:KDRvLVlt4hYOIPoEgbKFB8it5Www1FzamHMsJUmRDVM='),
 
     'previous_keys' => [
         ...array_filter(
